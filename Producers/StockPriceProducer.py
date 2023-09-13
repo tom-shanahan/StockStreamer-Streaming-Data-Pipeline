dec64b169f222700fca4b87031f4034370eb663b
tom-shanahan <tshanahan24@gmail.com>
2023-09-12 20:20:37 -0700

    StockPriceProducer uploading by record rather than ping. KafkaSteam to HDFS implimented

--- Producers/StockPriceProducer.py
+++ Producers/StockPriceProducer.py
@@ -1,6 +1,7 @@
 import json
 import os
 import io
+import re
 import websocket
 import avro.schema
 from avro.datafile import DataFileWriter
@@ -25,29 +26,36 @@ class StockPriceProducer:
                                     on_error = self.on_error,
                                     on_close = self.on_close)
         self.ws.on_open = self.on_open
+
         self.ws.run_forever()
         self.ws.on_close = self.on_close
 
         
     def on_message(self, ws, message):
-        
         message_json = json.loads(message)
         
         if message_json['type'] == "trade":
-            self.StockPriceOutput.append(message_json)
+            for singleRecord in message_json['data']:
+                self.StockPriceOutput.append(singleRecord)
 
-            with open("./Schemas/StockPriceSchema.avsc", "rb") as schema_file:
-                schema = avro.schema.parse(schema_file.read())
-            byteStream = io.BytesIO()
-            encoder = avro.io.BinaryEncoder(byteStream)
-            avro.io.DatumWriter(schema).write(message_json, encoder)
-            self.producer.send(self.KafkaTopic, byteStream.getvalue())
+                with open("./Schemas/StockPriceSchema.avsc", "rb") as schema_file:
+                    schema = avro.schema.parse(schema_file.read())
+                byteStream = io.BytesIO()
+                encoder = avro.io.BinaryEncoder(byteStream)
+                avro.io.DatumWriter(schema).write(singleRecord, encoder)
+
+                allowedKeyChars = r'[^a-zA-Z0-9\._\-]'
+                
+                self.producer.send(topic = self.KafkaTopic, 
+                                #    key = re.sub(allowedKeyChars, '', singleRecord['s']).encode('utf-8'), 
+                                    key = singleRecord['s'].encode('utf-8'), 
+                                   value = byteStream.getvalue())
 
 
     def on_error(self, ws, error):
         print(error)
-
         
+
     def on_close(self, ws, close_status_code, close_msg):
         print("on_close args:")
         if close_status_code or close_msg:
--- startKafkaServer.sh
+++ startKafkaServer.sh
@@ -5,14 +5,16 @@ KAFKA_CLUSTER_ID="$(./kafka_2.12-3.5.1/bin/kafka-storage.sh random-uuid)"
 
 
 # create kafka topics
+./kafka_2.12-3.5.1/bin/kafka-topics.sh --create --topic stockPrices --bootstrap-server localhost:9092
 ./kafka_2.12-3.5.1/bin/kafka-topics.sh --create --topic redditComments --bootstrap-server localhost:9092 
 ./kafka_2.12-3.5.1/bin/kafka-topics.sh --create --topic redditSubmissions --bootstrap-server localhost:9092 
-./kafka_2.12-3.5.1/bin/kafka-topics.sh --create --topic stockPrices --bootstrap-server localhost:9092
 
 
 # # test commands
 # # describe
 # ./kafka_2.12-3.5.1/bin/kafka-topics.sh --describe --topic stockPrices --bootstrap-server localhost:9092
+# ./kafka_2.12-3.5.1/bin/kafka-topics.sh --describe --topic redditSubmissions --bootstrap-server localhost:9092
+# ./kafka_2.12-3.5.1/bin/kafka-topics.sh --describe --topic redditComments --bootstrap-server localhost:9092
 
 # # start producer
 # ./kafka_2.12-3.5.1/bin/kafka-console-producer.sh --topic stockPrices --bootstrap-server localhost:9092
@@ -21,4 +23,6 @@ KAFKA_CLUSTER_ID="$(./kafka_2.12-3.5.1/bin/kafka-storage.sh random-uuid)"
 # ./kafka_2.12-3.5.1/bin/kafka-console-consumer.sh --topic stockPrices --from-beginning --bootstrap-server localhost:9092
 
 # # delete topic
+# ./kafka_2.12-3.5.1/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic stockPrices
+# ./kafka_2.12-3.5.1/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic redditSubmissions
 # ./kafka_2.12-3.5.1/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic redditComments
